3007b1235ff30c048be060dd48ca9b5c48498c0f
Kamil Jan Mularski <kammul@outlook.com>
2025-09-26 11:36:55 +0200

    My linked Stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,44 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static class Node<T> {
+        private final T value;
+        private Node<T> prev;
+
+        private Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private Node<T> top = null;
+    private int size = 0;
+
+    public void push(T value) {
+        Node<T> top = new Node<>(value);
+        top.prev = this.top;
+        this.top = top;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+        return top.value;
     }
 
     public T pop() {
-        return null;
+        T value = peek();
+        Node<T> currentTop = top;
+        top = top.prev;
+        currentTop.prev = null;
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
